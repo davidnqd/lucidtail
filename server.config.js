a5296bf47eae75f69cec32b3c943acb9a4efbeea
David Duong <david@nqd.me>
2013-05-02 13:23:54 -0600

    Massive update

--- server.config.js
+++ server.config.js
@@ -1,14 +1,15 @@
-var lucidLOG = require('./lib'),
-	SyslogUDP = require('./lib/SyslogUDP');
-
-var server = lucidLOG(new require('http').Server());
-
-var syslogListener = new SyslogUDP(514);
-server.listenTo(syslogListener);
-
-setInterval(function(){
-	server.emit('data', [{attributes: {source: "Test"}, text: "Test"}]);
-}, 1000);
-
-
+var asLucidGREP = require('./lib'),
+	server = asLucidGREP(new require('http').Server());
 server.listen(3000);
+
+var Tail = require('./lib/Tail');
+for (var i = 2; i < process.argv.length; i++) {
+	server.listenTo(new Tail(process.argv[i]), process.argv[i]);
+}
+
+var SyslogUDP = require('./lib/SyslogUDP');
+server.listenTo(new SyslogUDP(5140), "syslog-5140");
+
+var Test = require('./lib/Test');
+server.listenTo(new Test(), "Test 1");
+server.listenTo(new Test(), "Test 2");
