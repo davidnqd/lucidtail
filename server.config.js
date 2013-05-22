2b05f0347458ef82ecd8f56a456cd2ebf3d5c03c
David Duong <david.d@stratahealth.com>
2013-05-22 01:10:25 -0600

    DD: This is when I found out not to revert --hard

--- server.config.js
+++ server.config.js
@@ -1,15 +1,21 @@
-var asLucidGREP = require('./lib'),
-	server = asLucidGREP(new require('http').Server());
+var LucidGrep = require('./lib'),
+	server = new require('http').Server(),
+	lucid = new LucidGrep(server);
+
 server.listen(3000);
 
-var Tail = require('./lib/Tail');
+/*var Tail = require('./lib/Tail');
 for (var i = 2; i < process.argv.length; i++) {
-	server.listenTo(new Tail(process.argv[i]), process.argv[i]);
-}
+	lucid.listen(new Tail(process.argv[i]), process.argv[i]);
+}*/
 
-var SyslogUDP = require('./lib/SyslogUDP');
-server.listenTo(new SyslogUDP(514), "syslog-514");
+/*var SyslogUDP = require('./lib/SyslogUDP');
+lucid.listen(new SyslogUDP(514), "syslog-514");*/
 
 var Test = require('./lib/Test');
-server.listenTo(new Test(), "Test 1");
-server.listenTo(new Test(), "Test 2");
+lucid.listen(new Test(), "Test 1");
+lucid.listen(new Test(), "Test 2");
+
+lucid.on('error', function (err) {
+	console.error(err);
+});
